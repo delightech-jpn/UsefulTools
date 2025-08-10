a3e3cd67f7a7058ab90d9881d2c6383f415b9daa
delightech-jpn <delightech.jpn@gmail.com>
2025-08-10 22:18:31 +0900

    BuildError対応

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import requests
